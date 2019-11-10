241139e35a9f1bd39eb8c8fd98ff35e18ce855fe
Marcelo Barbosa Pinto <infobarbosa@yahoo.com.br>
2019-11-10 18:21:14 -0300

    alterando o tipo de controladora para SATA Controller e adicionando scylla_setup inline

--- lab1/install_scylla_centos_7.sh
+++ lab1/install_scylla_centos_7.sh
@@ -6,4 +6,4 @@ sudo wget -O /etc/yum.repos.d/scylla.repo http://repositories.scylladb.com/scyll
 
 sudo yum install -y scylla-enterprise-2018.1.6
 
-#scylla_setup
+sudo scylla_setup --disks /dev/md0 --nic eth1 --ntp-domain centos --setup-nic --no-ec2-check --no-cpuscaling-setup --no-fstrim-setup
