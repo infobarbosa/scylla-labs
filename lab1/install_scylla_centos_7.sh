212f5a466ed26d65a4da21e9ea9315713871a70d
Marcelo Barbosa Pinto <infobarbosa@yahoo.com.br>
2019-11-10 19:23:06 -0300

    instalando net-tools e python36-PyYAML.x86_64. Este último resolve o problema reportado em https://github.com/scylladb/scylla/issues/4379

--- lab1/install_scylla_centos_7.sh
+++ lab1/install_scylla_centos_7.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install -y epel-release wget
+sudo yum install -y epel-release wget net-tools
+sudo yum install -y python36-PyYAML.x86_64
 
 sudo wget -O /etc/yum.repos.d/scylla.repo http://repositories.scylladb.com/scylla/repo/false/centos/scylladb-2018.1.repo
 
